8a652ea3f4c32581489bf664e1b3900c7cb7c531
chinmayweb3 <144161893+chinmayweb3@users.noreply.github.com>
2024-06-28 23:15:12 +0530

    Home page template done

---
+++ src/home/index.tsx
@@ -0,0 +1,19 @@
+import FifthSection from "./FifthSection";
+import FourthSection from "./FourthSection";
+import FirstSection from "./firstSection";
+import SecondSection from "./secondSection";
+import ThirdSection from "./thirdSection";
+
+const Home = () => {
+  return (
+    <main className="text-black">
+      <FirstSection />
+      <SecondSection />
+      <ThirdSection />
+      <FourthSection />
+      <FifthSection />
+    </main>
+  );
+};
+
+export default Home;
