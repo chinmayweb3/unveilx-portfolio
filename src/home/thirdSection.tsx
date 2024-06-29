bebfbb94b67e082682712f9204b50c883049fa60
chinmayweb3 <144161893+chinmayweb3@users.noreply.github.com>
2024-06-29 15:01:15 +0530

    "Extracted repeated JSX into a new `Project` component and replaced duplicated code with it."

--- src/home/thirdSection.tsx
+++ src/home/thirdSection.tsx
@@ -20,6 +20,34 @@ const info = [
     src: "https://cdn.prod.website-files.com/6669563d598cbb2a4f785473/6669563d598cbb2a4f7854f1_4.webp",
   },
 ];
+type Iitem = {
+  item: {
+    title: string;
+    para: string;
+    src: string;
+  };
+};
+
+const Project = ({ item }: Iitem) => {
+  return (
+    <div
+      key={item.title}
+      className="relative rounded-xl w-full overflow-hidden group"
+    >
+      <img
+        className="aspect-[4:3] group-hover:scale-[1.1] duration-[700ms] ease-in-out "
+        src={item.src}
+      />
+      <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-[#0c0c0c05] to-[#000000c4] " />
+      <div className="absolute bottom-[5%] left-0 w-full text-center text-white">
+        <h2 className="uppercase font-Thunder text-[3.25rem] font-semibold leading-[1.1]">
+          {item.title}
+        </h2>
+        <p className="text-[1rem] mb-[16px]">{item.para}</p>
+      </div>
+    </div>
+  );
+};
 
 const ThirdSection = () => {
   return (
@@ -38,23 +66,9 @@ const ThirdSection = () => {
         </div>
         <div className="mt-[56px]">
           <div className="grid grid-cols-2 gap-x-[2rem] gap-y-[2.2rem] ">
-            {info.map((item) => {
-              return (
-                <div
-                  key={item.title}
-                  className="relative rounded-xl w-full overflow-hidden"
-                >
-                  <img className="aspect-[4:3] " src={item.src} />
-                  <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-[#0c0c0c05] to-[#000000c4] " />
-                  <div className="absolute bottom-[5%] left-0 w-full text-center text-white">
-                    <h2 className="uppercase font-Thunder text-[3.25rem] font-semibold leading-[1.1]">
-                      {item.title}
-                    </h2>
-                    <p className="text-[1rem] mb-[16px]">{item.para}</p>
-                  </div>
-                </div>
-              );
-            })}
+            {info.map((i) => (
+              <Project item={i} />
+            ))}
           </div>
         </div>
       </div>
