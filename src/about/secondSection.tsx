f3c6938e5465019a821a24f894c77908cb7af061
chinmayweb3 <144161893+chinmayweb3@users.noreply.github.com>
2024-07-01 15:16:41 +0530

    Updated SecondSection component to return a section with an image instead of a simple div

--- src/about/secondSection.tsx
+++ src/about/secondSection.tsx
@@ -1,5 +1,14 @@
 const SecondSection = () => {
-  return <div>SecondSection</div>;
+  return (
+    <section>
+      <div className="w-full h-auto overflow-hidden mb-[60px]">
+        <img
+          className="w-full"
+          src="https://cdn.prod.website-files.com/6669563d598cbb2a4f78544f/6669563d598cbb2a4f7854e2_Designer%2520Photo%2520final-p-3200.webp"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default SecondSection;
