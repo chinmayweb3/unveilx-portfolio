5eb77c960e1cba6795af2be4209aff582ac48043
chinmayweb3 <144161893+chinmayweb3@users.noreply.github.com>
2024-07-01 13:07:35 +0530

    Refactored navbar component to use a navList array and map function to generate links

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,28 +1,36 @@
 import { Link } from "react-router-dom";
 
+const navList = [
+  { name: "Home", path: "/" },
+  { name: "About", path: "/about" },
+  { name: "Work", path: "/work" },
+  { name: "Blog", path: "/blog" },
+];
+
 const Navbar = () => {
   return (
     <div className="w-full px-[60px] bg-black">
       <header className="mx-auto max-w-[1280px] h-[91px] flex items-center justify-between ">
         <Link to={"/"}>
-          <img src="https://cdn.prod.website-files.com/6669563d598cbb2a4f78544f/666b4b63ff948b19b104c941_Unveil%20X.svg" />
+          <img
+            className="w-[104px]"
+            src="https://cdn.prod.website-files.com/6669563d598cbb2a4f78544f/666b4b63ff948b19b104c941_Unveil%20X.svg"
+          />
         </Link>
         <nav className="flex items-center">
-          <Link className="px-[16px] py-[8px]" to={"/"}>
-            Home
-          </Link>
-          <Link className="px-[16px] py-[8px]" to={"/about"}>
-            About
-          </Link>
-          <Link className="px-[16px] py-[8px]" to={"/"}>
-            Work
-          </Link>
-          <Link className="px-[16px] py-[8px]" to={"/"}>
-            Blog
-          </Link>
-          <Link className="px-[16px] py-[8px]" to={"/"}>
-            Contact
-          </Link>
+          {navList.map((n, i) => {
+            return (
+              <Link
+                to={n.path}
+                key={i}
+                className={`px-[16px] py-[8px] ${
+                  n.path == location.pathname ? "" : "hover:underline"
+                }`}
+              >
+                {n.name}
+              </Link>
+            );
+          })}
         </nav>
       </header>
     </div>
