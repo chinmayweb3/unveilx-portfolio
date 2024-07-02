e113791551119a5d243a37867f57c9ffbcaf90fb
chinmayweb3 <144161893+chinmayweb3@users.noreply.github.com>
2024-07-03 00:03:32 +0530

    Refactored FirstSection component: updated imports, class names, and styling for section, header, paragraph, and button elements

--- src/about/firstSection.tsx
+++ src/about/firstSection.tsx
@@ -1,24 +1,22 @@
-import { ButtonField } from "../components/buttons";
+import { ButtonField } from '../components/buttons';
 
 const FirstSection = () => {
   return (
-    <section className="w-full px-[60px] bg-gray2 text-black pb-[60px] ">
-      <div className="max-w-[1280px] mx-auto py-[2rem]">
-        <div className="flex flex-col justify-start text-center items-center">
-          <h1 className="uppercase font-Thunder text-[10rem] mlg:text-[7rem] leading-[1.1] font-semibold pb-[16px] mt-[20px] mb-[10px] ">
-            bringing{" "}
-            <span className="inline-block">
-              <img src="./header.webp" />
-            </span>{" "}
+    <section className="w-full bg-gray2 px-[60px] pb-[60px] text-black msm:px-[6%] msm:pb-[40px]">
+      <div className="mx-auto max-w-[1280px] py-[2rem]">
+        <div className="flex flex-col items-center justify-start text-center">
+          <h1 className="mb-[10px] mt-[20px] pb-[16px] font-Thunder text-[10rem] font-semibold uppercase leading-[1.1] mlg:text-[7rem] msm:text-[3rem]">
+            bringing{' '}
+            <span className="inline-block h-[40px]">
+              <img className="msm:h-full" src="./header.webp" />
+            </span>{' '}
             best design solutions
           </h1>
-          <p className="max-w-[56rem] text-[1.5rem] text-gray3 ">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            varius enim in eros elementum tristique. Duis cursus, mi quis
-            viverra ornare, eros dolor interdum nulla, ut commodo diam libero
-            vitae erat.
+          <p className="max-w-[56rem] text-[1.5rem] text-gray3 msm:text-[1.25rem]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis
+            cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.
           </p>
-          <div className="flex gap-[12px] mt-[40px] border-black">
+          <div className="mt-[40px] flex gap-[12px] border-black">
             <ButtonField>behance</ButtonField>
             <ButtonField>dribble</ButtonField>
             <ButtonField>twitter</ButtonField>
