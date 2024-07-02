3c09a201ddcc4d67a5d7a9daadf3a86c1e4b39b6
chinmayweb3 <144161893+chinmayweb3@users.noreply.github.com>
2024-07-03 00:14:06 +0530

    Refactored fourthSection.tsx and secondSection.tsx: updated CSS classes, removed unnecessary imports, and reformatted code

--- src/about/fourthSection.tsx
+++ src/about/fourthSection.tsx
@@ -1,48 +1,39 @@
 const info = [
   {
-    id: "01",
-    title: "research",
+    id: '01',
+    title: 'research',
     para: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
   },
   {
-    id: "02",
-    title: "WIREFRAMING",
+    id: '02',
+    title: 'WIREFRAMING',
     para: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
   },
   {
-    id: "03",
-    title: "VISUAL DESIGN",
+    id: '03',
+    title: 'VISUAL DESIGN',
     para: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
   },
   {
-    id: "04",
-    title: "TESTING",
+    id: '04',
+    title: 'TESTING',
     para: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
   },
 ];
 const FourthSection = () => {
   return (
-    <section className="w-full px-[60px] py-[112px] bg-black text-white">
-      <div className="max-w-[1280px] mx-auto">
-        <div className="grid grid-cols-2 gap-[3rem] ">
+    <section className="w-full bg-black px-[60px] py-[112px] text-white msm:px-[6%] msm:py-[4rem]">
+      <div className="mx-auto max-w-[1280px]">
+        <div className="grid grid-cols-2 gap-[3rem] msm:grid-cols-1">
           <div className="max-w-[24rem]">
-            <h2 className="sticky top-[80px] uppercase font-Thunder text-[5.5rem] leading-[1.1]">
-              my work process
-            </h2>
+            <h2 className="sticky top-[80px] font-Thunder text-[5.5rem] uppercase leading-[1.1]">my work process</h2>
           </div>
           <div className="flex flex-col gap-[32px]">
             {info.map((item) => (
-              <div
-                key={item.id}
-                className="sticky top-[80px] py-[36px] px-[32px] rounded-[8px] border bg-gray3 border-[#424242]"
-              >
-                <h2 className="uppercase font-Thunder font-semibold leading-[1.1] text-[5rem] mb-[1rem]">
-                  {item.id}
-                </h2>
-                <div className="uppercase text-[2rem] font-medium mb-[1rem] ">
-                  {item.title}
-                </div>
-                <p className="leading-[1.7em] text-[1rem]  ">{item.para}</p>
+              <div key={item.id} className="sticky top-[80px] rounded-[8px] border border-[#424242] bg-gray3 px-[32px] py-[36px]">
+                <h2 className="mb-[1rem] font-Thunder text-[5rem] font-semibold uppercase leading-[1.1]">{item.id}</h2>
+                <div className="mb-[1rem] text-[2rem] font-medium uppercase">{item.title}</div>
+                <p className="text-[1rem] leading-[1.7em]">{item.para}</p>
               </div>
             ))}
           </div>
--- src/about/secondSection.tsx
+++ src/about/secondSection.tsx
@@ -1,23 +1,19 @@
-import React from "react";
-import { motion, useScroll, useTransform } from "framer-motion";
+import React from 'react';
+import { motion, useScroll, useTransform } from 'framer-motion';
 
 const SecondSection = () => {
   const divRef = React.useRef(null);
   const { scrollYProgress } = useScroll({
     target: divRef,
-    offset: ["start center", "end start"],
+    offset: ['start center', 'end start'],
   });
 
   const scale = useTransform(scrollYProgress, [0, 1], [1, 1.4]);
 
   return (
-    <section className="overflow-">
-      <div ref={divRef} className="w-full h-auto overflow-hidden mb-[60px]">
-        <motion.img
-          style={{ scale }}
-          className="w-full inline-block duration-100"
-          src="./aboutbg.webp"
-        />
+    <section>
+      <div ref={divRef} className="mb-[60px] h-auto w-full overflow-hidden">
+        <motion.img style={{ scale }} className="inline-block w-full duration-100" src="./aboutbg.webp" />
       </div>
     </section>
   );
