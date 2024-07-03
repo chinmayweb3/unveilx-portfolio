0901b9b9bcd80da678c09789bc20d3d9719ceede
chinmayweb3 <144161893+chinmayweb3@users.noreply.github.com>
2024-07-03 14:22:12 +0530

    Added Translation component to ThirdSection, wrapping h2 and div elements

--- src/about/thirdSection.tsx
+++ src/about/thirdSection.tsx
@@ -1,3 +1,5 @@
+import Translation from '../components/translation';
+
 const info = [
   {
     date: '2010 - 2012',
@@ -21,7 +23,9 @@ const ThirdSection = () => {
     <section className="w-full bg-black px-[60px] py-[112px] text-white msm:px-[6%] msm:py-[4rem]">
       <div className="mx-auto max-w-[1280px]">
         <div className="grid grid-cols-[0.75fr_1.5fr] gap-[2.5rem] msm:grid-cols-1">
-          <h2 className="uppercase">About me</h2>
+          <Translation sideRun="-y">
+            <h2 className="uppercase">About me</h2>
+          </Translation>
           <div className="border-white font-normal">
             <p className="mb-[50px] text-[1.25rem] leading-[1.5em] msm:text-[1rem]">
               I have been building websites for more than 7 years. With a passion for crafting visually stunning websites, I've
@@ -31,7 +35,8 @@ const ThirdSection = () => {
             <p className="mb-[1.5rem] text-[1rem] leading-[1.5em]">My work history</p>
 
             {info.map((n, i) => (
-              <div
+              <Translation
+                sideRun="-y"
                 key={n.title}
                 className={`flex items-center justify-between border-b msm:flex-col msm:items-start msm:gap-0 ${i == 0 ? 'border-t' : ''}`}
               >
@@ -40,7 +45,7 @@ const ThirdSection = () => {
                   <div className="font-Thunder text-[2.75rem] font-medium uppercase leading-[1.1] msm:text-[2rem]">{n.title}</div>
                 </div>
                 <div className="text-[1rem] font-normal leading-[1.7em] msm:mb-[20px]">{n.subTitle}</div>
-              </div>
+              </Translation>
             ))}
           </div>
         </div>
