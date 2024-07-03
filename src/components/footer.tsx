32f4b9af4cdc373c7ca9fe616ebc194fb7e76c93
chinmayweb3 <144161893+chinmayweb3@users.noreply.github.com>
2024-07-03 14:06:06 +0530

    Wrapped footer elements with Translation component

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,16 +1,24 @@
+import Translation from './translation';
+
 const footList = ['Licenses', 'Style guide', 'About ', 'Changelog', 'Instructions', '404 page', 'Protected Page', 'Contact'];
 const Footer = () => {
   return (
     <footer className="w-full bg-lime-green px-[60px] py-[32px] text-black msm:px-[6%]">
       <div className="mx-auto max-w-[1280px] pb-[16px]">
         <div className="py-[80px] text-center msm:py-[60px]">
-          <p className="pb-[20px] text-[1.25rem] font-normal msm:text-[1rem]">Have a project in mind?</p>
-          <div className="mb-[10px] font-Thunder text-[10rem] font-semibold uppercase leading-[1.1] msm:text-[3rem]">
-            LET'S WORK TOGETHER
-          </div>
-          <button className="mx-auto rounded-[12rem] border border-black px-[2rem] py-[1rem] text-[1.125rem] uppercase duration-100 hover:bg-black hover:text-white">
-            get in touch
-          </button>
+          <Translation sideRun="-y" sidePx={70}>
+            <p className="pb-[20px] text-[1.25rem] font-normal msm:text-[1rem]">Have a project in mind?</p>
+          </Translation>
+          <Translation sideRun="-y" sidePx={70}>
+            <h2 className="mb-[10px] font-Thunder text-[10rem] font-semibold uppercase leading-[1.1] msm:text-[3rem]">
+              LET'S WORK TOGETHER
+            </h2>
+          </Translation>
+          <Translation sideRun="-y" sidePx={70}>
+            <button className="mx-auto rounded-[12rem] border border-black px-[2rem] py-[1rem] text-[1.125rem] uppercase duration-100 hover:bg-black hover:text-white">
+              get in touch
+            </button>
+          </Translation>
         </div>
         <div className="h-[2px] w-full bg-black" />
         <div className="flex items-center justify-between pt-[48px] text-[1rem] msm:flex-col msm:gap-[24px] msm:text-[0.85rem]">
