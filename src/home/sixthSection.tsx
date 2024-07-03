a43bc933f83e32fb56645c1804596a2de5641afe
chinmayweb3 <144161893+chinmayweb3@users.noreply.github.com>
2024-07-03 14:03:05 +0530

    Added Translation component to sixthSection.tsx, wrapping certain elements with it

--- src/home/sixthSection.tsx
+++ src/home/sixthSection.tsx
@@ -1,3 +1,5 @@
+import Translation from '../components/translation';
+
 const blogInfo = [
   {
     title: 'Design',
@@ -20,13 +22,13 @@ const Blog = () => (
   <>
     {blogInfo.map((t, i) => {
       return (
-        <div key={t.title + i} className="flex flex-col gap-[4px]">
+        <Translation sideRun="-y" sidePx={50} key={t.title + i} className="flex flex-col gap-[4px]">
           <button className="mb-[0.5rem] w-fit rounded-[2rem] border-[0.5px] border-white px-[10px] py-[4px] text-[0.82rem]">
             {t.title}
           </button>
           <h3 className="font-Thunder text-[2.75rem] font-medium leading-[1] msm:mr-[20px] msm:text-[2rem]">{t.para}</h3>
           <p className="text-[1rem] text-gray1">{t.date}</p>
-        </div>
+        </Translation>
       );
     })}
   </>
@@ -36,7 +38,9 @@ const SixthSection = () => {
     <section className="w-full bg-black px-[60px] pb-[112px] text-white msm:px-[6%] msm:pb-[4rem]">
       <div className="mx-auto max-w-[1280px]">
         <div>
-          <h2 className="mb-[3.15rem] font-Thunder text-[5rem] font-semibold uppercase msm:text-[2.85rem]">latest from blog</h2>
+          <Translation sideRun="-y">
+            <h2 className="mb-[3.15rem] font-Thunder text-[5rem] font-semibold uppercase msm:text-[2.85rem]">latest from blog</h2>
+          </Translation>
           <div className="grid grid-cols-3 gap-x-[32px] msm:grid-cols-1 msm:gap-[24px]">
             <Blog />
           </div>
