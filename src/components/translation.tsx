3784a212dbe1038e97c477dae98b72e0310ed8c2
chinmayweb3 <144161893+chinmayweb3@users.noreply.github.com>
2024-07-03 12:56:14 +0530

    Wrapped h1 element with Translation component in FirstSection.tsx

---
+++ src/components/translation.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import { twMerge } from 'tailwind-merge';
+
+// type buttonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
+type divProps = React.HTMLAttributes<HTMLDivElement> & {
+  sideRun?: 'x' | 'y' | '-x' | '-y';
+};
+
+const Translation = React.forwardRef<HTMLDivElement, divProps>(({ sideRun = 'x', ...props }, ref) => {
+  const position = sideRun.startsWith('-');
+  let side = position ? sideRun[1] : sideRun[0];
+  return (
+    <motion.div
+      ref={ref}
+      initial={{
+        [side]: `${position ? '' : '-'}100px`,
+        opacity: 0,
+      }}
+      whileInView={{
+        [side]: 0,
+        opacity: 1,
+      }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.5 }}
+      className={twMerge('', props.className)}
+    >
+      {props.children}
+    </motion.div>
+  );
+});
+
+export default Translation;
