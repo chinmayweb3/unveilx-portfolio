2d08c3cf1a47c29aa7afdf20ab09d9669ca03861
chinmayweb3 <144161893+chinmayweb3@users.noreply.github.com>
2024-07-03 13:09:15 +0530

    Removed unnecessary Translation components and adjusted nesting

--- src/home/firstSection.tsx
+++ src/home/firstSection.tsx
@@ -24,20 +24,21 @@ const FirstSection = () => {
             cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.
           </p>
         </div>
-        <div className="mt-[150px] flex items-center justify-between gap-[32px] msm:mt-[60px] msm:flex-col msm:items-start">
+        <Translation
+          sideRun="-y"
+          className="mt-[150px] flex items-center justify-between gap-[32px] msm:mt-[60px] msm:flex-col msm:items-start"
+        >
           <div>
-            <Translation sideRun="-y">
-              <button className="flex items-center justify-center gap-[32px] border-b-[1px] border-white pb-[10px] text-[1.15rem] duration-100 hover:border-lime-green hover:text-lime-green">
-                <span>Get in touch</span> <span className="font-black">↗︎</span>
-              </button>
-            </Translation>
+            <button className="flex items-center justify-center gap-[32px] border-b-[1px] border-white pb-[10px] text-[1.15rem] duration-100 hover:border-lime-green hover:text-lime-green">
+              <span>Get in touch</span> <span className="font-black">↗︎</span>
+            </button>
           </div>
-          <Translation sideRun="-y" className="flex justify-end gap-[12px] uppercase">
+          <div className="flex justify-end gap-[12px] uppercase">
             <ButtonField className="border-white py-[10px] hover:border-lime-green hover:text-lime-green">behance</ButtonField>
             <ButtonField className="border-white py-[10px] hover:border-lime-green hover:text-lime-green">dribble</ButtonField>
             <ButtonField className="border-white py-[10px] hover:border-lime-green hover:text-lime-green">twitter</ButtonField>
-          </Translation>
-        </div>
+          </div>
+        </Translation>
       </div>
     </section>
   );
