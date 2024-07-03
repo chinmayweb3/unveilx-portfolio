21e68d54e330a1729a1be646ac80e80a685e6edb
chinmayweb3 <144161893+chinmayweb3@users.noreply.github.com>
2024-07-03 13:06:01 +0530

    Updated Translation component to accept sidePx prop and refactored its usage in FirstSection component

--- src/components/translation.tsx
+++ src/components/translation.tsx
@@ -5,16 +5,17 @@ import { twMerge } from 'tailwind-merge';
 // type buttonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
 type divProps = React.HTMLAttributes<HTMLDivElement> & {
   sideRun?: 'x' | 'y' | '-x' | '-y';
+  sidePx?: number;
 };
 
-const Translation = React.forwardRef<HTMLDivElement, divProps>(({ sideRun = 'x', ...props }, ref) => {
+const Translation = React.forwardRef<HTMLDivElement, divProps>(({ sidePx = 100, sideRun = 'x', ...props }, ref) => {
   const position = sideRun.startsWith('-');
   let side = position ? sideRun[1] : sideRun[0];
   return (
     <motion.div
       ref={ref}
       initial={{
-        [side]: `${position ? '' : '-'}100px`,
+        [side]: `${position ? '' : '-'}${sidePx}px`,
         opacity: 0,
       }}
       whileInView={{
--- src/home/firstSection.tsx
+++ src/home/firstSection.tsx
@@ -6,19 +6,19 @@ const FirstSection = () => {
     <section className="w-full bg-black px-[60px] pb-[112px] pt-[84px] msm:px-[6%] msm:py-[4rem] msm:pt-[1rem]">
       <div className="mx-auto max-w-[1280px]">
         <div className="w-[90%]">
-          <Translation sideRun="x">
-            <h1 className="mb-[10px] mt-[20px] font-Thunder text-[10rem] font-semibold uppercase leading-[1.1] mlg:text-[7rem] msm:text-[3rem]">
-              i'm John smith{' '}
-              <span className="inline-block msm:h-[40px]">
-                <img className="msm:h-full" src="./header.webp" />
-              </span>{' '}
-              product designer & web{' '}
-              <span className="inline-block msm:h-[40px]">
-                <img className="msm:h-full" src="./headerstar.webp" />
-              </span>{' '}
-              developer
-            </h1>
+          {/* <Translation sideRun="x"> */}
+          <Translation className="mb-[10px] mt-[20px] font-Thunder text-[10rem] font-semibold uppercase leading-[1.1] mlg:text-[7rem] msm:text-[3rem]">
+            i'm John smith{' '}
+            <span className="inline-block msm:h-[40px]">
+              <img className="msm:h-full" src="./header.webp" />
+            </span>{' '}
+            product designer & web{' '}
+            <span className="inline-block msm:h-[40px]">
+              <img className="msm:h-full" src="./headerstar.webp" />
+            </span>{' '}
+            developer
           </Translation>
+          {/* </Translation> */}
           <p className="max-w-[64rem] text-[2rem] text-gray1 msm:text-[1.25rem]">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis
             cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.
@@ -26,15 +26,17 @@ const FirstSection = () => {
         </div>
         <div className="mt-[150px] flex items-center justify-between gap-[32px] msm:mt-[60px] msm:flex-col msm:items-start">
           <div>
-            <button className="flex items-center justify-center gap-[32px] border-b-[1px] border-white pb-[10px] text-[1.15rem] duration-100 hover:border-lime-green hover:text-lime-green">
-              <span>Get in touch</span> <span className="font-black">↗︎</span>
-            </button>
+            <Translation sideRun="-y">
+              <button className="flex items-center justify-center gap-[32px] border-b-[1px] border-white pb-[10px] text-[1.15rem] duration-100 hover:border-lime-green hover:text-lime-green">
+                <span>Get in touch</span> <span className="font-black">↗︎</span>
+              </button>
+            </Translation>
           </div>
-          <div className="flex justify-end gap-[12px] uppercase">
+          <Translation sideRun="-y" className="flex justify-end gap-[12px] uppercase">
             <ButtonField className="border-white py-[10px] hover:border-lime-green hover:text-lime-green">behance</ButtonField>
             <ButtonField className="border-white py-[10px] hover:border-lime-green hover:text-lime-green">dribble</ButtonField>
             <ButtonField className="border-white py-[10px] hover:border-lime-green hover:text-lime-green">twitter</ButtonField>
-          </div>
+          </Translation>
         </div>
       </div>
     </section>
