bb0d80eea5a2f0e1758facd68c333a9f54d01116
chinmayweb3 <144161893+chinmayweb3@users.noreply.github.com>
2024-07-03 13:38:50 +0530

    "Updated SecondSection component: added duration prop to Translation, changed sidePx value, replaced div with h2, and reformatted code"

--- src/home/secondSection.tsx
+++ src/home/secondSection.tsx
@@ -4,8 +4,13 @@ const SecondSection = () => {
   return (
     <section className="w-full bg-gray2 px-[60px] py-[112px] text-black msm:px-[6%] msm:py-[4rem]">
       <div className="mx-auto max-w-[1280px]">
-        <Translation sideRun="-y" sidePx={80} className="msm: grid w-full grid-cols-[0.75fr_1fr] msm:grid-cols-1 msm:gap-[2rem]">
-          <div className="text-[1rem] uppercase">About Me</div>
+        <Translation
+          sideRun="-y"
+          duration={0.5}
+          sidePx={40}
+          className="msm: grid w-full grid-cols-[0.75fr_1fr] msm:grid-cols-1 msm:gap-[2rem]"
+        >
+          <h2 className="text-[1rem] uppercase">About Me</h2>
           <div className="max-w-[600px]">
             <p className="text-[1.25rem] leading-[1.5em] msm:text-[1rem]">
               I partner with clients worldwide to cultivate captivating and immersive digital experiences through the realms of
